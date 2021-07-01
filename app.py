98445a6ae2740dbdc90df7a1cb1f4bc1b99d9436
Squeekoo <edczjr@gmail.com>
2021-07-01 12:51:23 -0700

    cleaned up HTML, worked About page, and some styling

--- app.py
+++ app.py
@@ -9,4 +9,11 @@ app = Flask(__name__)
 def homepage():
     """ Portfolio Homepage """
 
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    """ About Page """
+
+    return render_template("about.html")
